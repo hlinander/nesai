b65897a281ec580a22a51168f775cdf12f63dcb5
Hampus Linander <hampus.linander@zenuity.com>
2020-01-17 20:35:24 +0100

    Actually reintroduce death on idle
    Fix reset resume conditionals

--- nodemind/scripts/smb.lua
+++ nodemind/scripts/smb.lua
@@ -103,26 +103,26 @@ function brain_get_reward(frame)
 			reward = reward + dscore
 			old_score = mario_score
         end
-		-- if math.abs(last_absolute_x - absolute_x) < 10 then
-		-- 	idle_frames = idle_frames + 1
-		-- 	if idle_frames > 120 then
-		-- 		reward = reward - 1
-		-- 		if idle_frames > 180 then
-		-- 			-- is_dead = true
-		-- 			if nloads < 5 then
-		-- 				load_state()
-		-- 				nloads = nloads + 1
-		-- 			else
-		-- 				is_dead = true
-		-- 			end
-		-- 			reward = reward - 100
-		-- 			next_save_frame = 0
-		-- 		end
-		-- 	end
-		-- else
-		-- 	idle_frames = 0
-		-- 	last_absolute_x = absolute_x
-		-- end
+		if math.abs(last_absolute_x - absolute_x) < 10 then
+			idle_frames = idle_frames + 1
+			if idle_frames > 120 then
+				reward = reward - 1
+				if idle_frames > 180 then
+					is_dead = true
+					-- if nloads < 5 then
+					-- 	load_state()
+					-- 	nloads = nloads + 1
+					-- else
+					-- 	is_dead = true
+					-- end
+					reward = reward - 100
+					next_save_frame = 0
+				end
+			end
+		else
+			idle_frames = 0
+			last_absolute_x = absolute_x
+		end
 	else
 		last_absolute_x = absolute_x
 		idle_frames = 0
